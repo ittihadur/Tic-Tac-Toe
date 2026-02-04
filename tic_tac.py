d238097a85404eb58a4bc0be4454aa20540b4c35
Ittihadur <ittihadur@gmail.com>
2026-02-04 13:20:19 +0600

    First commit: Did the menu

---
+++ tic_tac.py
@@ -0,0 +1,32 @@
+# Starting menu 
+
+print("Welcome to Tic Tac Toe")
+'''
+print("Choose game mode using numbers \n ")
+print("1) Player vs Player")
+print("2) Player vs computer")
+input_by_player = input()
+if input_by_player.isdigit() == True:
+    conversion = int(input_by_player)
+    if conversion == 1:
+        print("Player vs Player")
+    elif conversion == 2:
+        print("Player vs Computer")
+    else:
+        print("Invalid Choice")
+else:
+    print("Invalid choice")
+'''
+
+#Creating the board.
+
+print("Press X and O to play")
+
+board = [i * 1 for i in range(1,10)]
+
+print(board[0:3])
+print
+
+
+
+
