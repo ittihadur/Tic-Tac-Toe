07e0a9a2a3ee453efcb1961513954d0156a8f268
Ittihadur <ittihadur@gmail.com>
2026-02-05 13:03:47 +0600

    Updated the code so that loop runs continiously and did the board. will update the indexing later

--- tic_tac.py
+++ tic_tac.py
@@ -1,31 +1,67 @@
 # Starting menu 
 
 print("Welcome to Tic Tac Toe")
-'''
+
 print("Choose game mode using numbers \n ")
 print("1) Player vs Player")
 print("2) Player vs computer")
-input_by_player = input()
-if input_by_player.isdigit() == True:
-    conversion = int(input_by_player)
-    if conversion == 1:
-        print("Player vs Player")
-    elif conversion == 2:
-        print("Player vs Computer")
+while True:
+    input_by_player = input()
+    if input_by_player.isdigit() == True:
+        conversion = int(input_by_player)
+        if conversion == 1:
+            print("Player vs Player")
+            break
+        elif conversion == 2:
+            print("Player vs Computer")
+            break
+        else:
+            print("Invalid Choice")
+            continue
     else:
-        print("Invalid Choice")
-else:
-    print("Invalid choice")
-'''
+        print("Invalid choice")
+        continue
+
 
 #Creating the board.
 
 print("Press X and O to play")
 
-board = [i * 1 for i in range(1,10)]
+board = [i * 1 for i in range(0,9)]
 
-print(board[0:3])
-print
+
+print("+" + "---+" * 3)
+for i in range(0, 9, 3):
+    print("|", end="")
+    for j in range(3):
+        print(f" {board[i+j]} |", end="")
+    print()
+    print("+" + "---+" * 3)
+
+#place_X = input("Press x ONLY!! :")
+
+while True:
+    place_for_X = input("Choose froom position 0 to 8 to place X: " )
+    if place_for_X.isdigit() == True:
+        conversion = int(place_for_X)
+        if conversion < 0 or conversion > 8:
+            print("Write within the parameter 1 to 9 u dummy")
+            continue
+        
+        else:
+            print(f"Valid Input {conversion}")
+            break
+    else:
+        print("Just a number bruv")
+        continue
+
+
+    
+
+
+
+
+
 
 
 
